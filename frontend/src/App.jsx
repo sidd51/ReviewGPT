f030ae12765d6e94ab6ddf40d2f7b95fd0306581
sidd51 <siddhiborawake2004@gmail.com>
2026-02-09 23:08:17 +0530

    Setup frontend layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-   
-    </>
-  )
+    <main>
+      <div className="left">
+        <div className="code"></div>
+        <div className="review">Review</div>
+      </div>
+
+      <div className="right"></div>
+    </main>
+  );
 }
 
-export default App
+export default App;
